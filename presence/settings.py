03adacc87331967e4b92460768a24f1316de392a
Harish Navnit <harishnavnit@gmail.com>
2024-05-07 11:05:32 +0800

    Use postgres db

--- presence/settings.py
+++ presence/settings.py
@@ -77,8 +77,13 @@ WSGI_APPLICATION = 'presence.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'presence',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+        'TEST': {
+            'NAME': 'testing/presence'
+        }
     }
 }
 
