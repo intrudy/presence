9312ae034f5039596697057b30a7bb137f2e94f9
Harish Navnit <harishnavnit@gmail.com>
2024-05-09 19:44:48 +0800

    Fix errors

--- hosts/urls.py
+++ hosts/urls.py
@@ -9,14 +9,17 @@ urlpatterns = [
     path("hosts/<str:uid>", views.hosts.fetch, name="View Host details"),
     path("hosts/<str:uid>/edit", views.hosts.edit, name="Edit host details"),
     path("hosts/<str:uid>/delete", views.hosts.delete, name="Delete a host"),
+    path("hosts/register", views.hosts.register, name='Register a new host'),
 
     path("cohorts", views.cohorts.filter, name="Cohort history"),
-    path("cohorts/<str:uid>", views.cohorts.fetch, name="Cohort details"),
-    path("cohorts/<str:uid>/edit", views.cohorts.edit, name="Edit Cohort detail"),
-    path("cohorts/<str:uid>/delete", views.cohorts.delete, name="Delete a Cohort"),
+    path("cohorts/<str:uid>", views.cohorts.fetch, name="View cohort details"),
+    path("cohorts/<str:uid>/edit", views.cohorts.edit, name="Edit cohort detail"),
+    path("cohorts/<str:uid>/delete", views.cohorts.delete, name="Delete a cohort"),
+    path("cohorts/register", views.cohorts.register, name="Register a new cohort"),
 
-    path("events", views.events, name="Event history"),
+    path("events", views.events.filter, name="Event history"),
     path("events/<str:uid>", views.events.fetch, name="Event details"),
     path("events/<str:uid>/edit", views.events.edit, name="Edit event details"),
-    path("events/<str:uid>/delete", views.events.delete, )
+    path("events/<str:uid>/delete", views.events.delete, name="Delete an event"),
+    path("events/register", views.events.register, name="Register a new event")
 ]
