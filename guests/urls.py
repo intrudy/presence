1761137beea5d5a405f2ad94bda6aa5eb78f7c07
Harish Navnit <harishnavnit@gmail.com>
2024-05-11 00:57:44 +0800

    update urls.py

--- guests/urls.py
+++ guests/urls.py
@@ -1,23 +1,28 @@
 
 from django.urls import path
 
-from . import views
+from .views.guests import Guest
+from .views.guests import Guests
+from .views.markers import Marker
+from .views.markers import Markers
+from .views.teams import Team
+from .views.teams import Teams
 
 
 urlpatterns = [
-    path("guests", views.guests.filter, name="View all guests"),
-    path("guests/<str:uid>", views.guests.fetch, name="View guest details"),
-    path("guests/<str:uid>/edit", views.guests.edit, name="Edit guest details"),
-    path("guests/register", views.guests.register, name="Register new guest profile"),
+    path("guests", Guests.as_view(), name="View all guests"),
+    path("guests/<str:uid>", Guest.as_view(), name="View guest details"),
+    path("guests/<str:uid>/edit", Guest.as_view(), name="Edit guest details"),
+    path("guests/register", Guest.as_view(), name="Register new guest profile"),
 
-    path("markers", views.markers.filter, name="Filter attendance markers"),
-    path("markers/<str:uid>", views.markers.fetch, name="Attendance marker details"),
-    path("markers/<str:uid>/edit", views.markers.edit, name="Edit attendance marker"),
-    path("markers/<str:uid>/delete", views.markers.delete, name="Delete attendance marker"),
-    path("markers/register", views.markers.register, name="Create attendance marker"),
+    path("markers", Markers.as_view(), name="Filter attendance markers"),
+    path("markers/<str:uid>", Marker.as_view(), name="Attendance marker details"),
+    path("markers/<str:uid>/edit", Marker.as_view(), name="Edit attendance marker"),
+    path("markers/<str:uid>/delete", Marker.as_view(), name="Delete attendance marker"),
+    path("markers/register", Marker.as_view(), name="Create attendance marker"),
 
-    path("teams", views.teams.filter, name="View all teams"),
-    path("teams/<str:uid>", views.teams.fetch, name="View team details"),
-    path("teams/<str:uid>/edit", views.teams.edit, name="Edit team details"),
-    path("teams/register", views.teams.register, name="Register a new team")
+    path("teams", Teams.as_view(), name="View all teams"),
+    path("teams/<str:uid>", Team.as_view(), name="View team details"),
+    path("teams/<str:uid>/edit", Team.as_view(), name="Edit team details"),
+    path("teams/register", Team.as_view(), name="Register a new team")
 ]
