990480afd8ffc0412f2c950ab4dbc4c057b2c53e
Harish Navnit <harishnavnit@gmail.com>
2024-05-11 00:57:44 +0800

    update urls.py

--- hosts/urls.py
+++ hosts/urls.py
@@ -1,25 +1,30 @@
 
 from django.urls import path
 
-from . import views
+from .views.cohorts import Cohort
+from .views.cohorts import Cohorts
+from .views.events import Event
+from .views.events import Events
+from .views.hosts import Host
+from .views.hosts import Hosts
 
 
 urlpatterns = [
-    path("hosts", views.hosts.filter, name="Hosts"),
-    path("hosts/<str:uid>", views.hosts.fetch, name="View Host details"),
-    path("hosts/<str:uid>/edit", views.hosts.edit, name="Edit host details"),
-    path("hosts/<str:uid>/delete", views.hosts.delete, name="Delete a host"),
-    path("hosts/register", views.hosts.register, name='Register a new host'),
+    path("hosts", Hosts.as_view(), name="Hosts"),
+    path("hosts/<str:uid>", Host.as_view(), name="View Host details"),
+    path("hosts/<str:uid>/edit", Host.as_view(), name="Edit host details"),
+    path("hosts/<str:uid>/delete", Host.as_view(), name="Delete a host"),
+    path("hosts/register", Host.as_view(), name='Register a new host'),
 
-    path("cohorts", views.cohorts.filter, name="Cohort history"),
-    path("cohorts/<str:uid>", views.cohorts.fetch, name="View cohort details"),
-    path("cohorts/<str:uid>/edit", views.cohorts.edit, name="Edit cohort detail"),
-    path("cohorts/<str:uid>/delete", views.cohorts.delete, name="Delete a cohort"),
-    path("cohorts/register", views.cohorts.register, name="Register a new cohort"),
+    path("cohorts", Cohorts.as_view(), name="Cohort history"),
+    path("cohorts/<str:uid>", Cohort.as_view(), name="View cohort details"),
+    path("cohorts/<str:uid>/edit", Cohort.as_view(), name="Edit cohort detail"),
+    path("cohorts/<str:uid>/delete", Cohort.as_view(), name="Delete a cohort"),
+    path("cohorts/register", Cohort.as_view(), name="Register a new cohort"),
 
-    path("events", views.events.filter, name="Event history"),
-    path("events/<str:uid>", views.events.fetch, name="Event details"),
-    path("events/<str:uid>/edit", views.events.edit, name="Edit event details"),
-    path("events/<str:uid>/delete", views.events.delete, name="Delete an event"),
-    path("events/register", views.events.register, name="Register a new event")
+    path("events", Events.as_view(), name="Event history"),
+    path("events/<str:uid>", Event.as_view(), name="Event details"),
+    path("events/<str:uid>/edit", Event.as_view(), name="Edit event details"),
+    path("events/<str:uid>/delete", Event.as_view(), name="Delete an event"),
+    path("events/register", Event.as_view(), name="Register a new event")
 ]
