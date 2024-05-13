96cb2057399f335dd137f4e33a3b7d59bc4e2145
Harish Navnit <harishnavnit@gmail.com>
2024-05-13 23:25:16 +0800

    Define components

--- dashboard/src/app/App.js
+++ dashboard/src/app/App.js
@@ -1,16 +1,14 @@
 
-import '../css/App.css'
-import logo from '../logo.svg'
+import React from 'react'
+import Header from './components/Header'
+import Login from './components/Login'
 
 
 export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Edit <code>src/app/App.js</code> and save to reload.</p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">Learn React</a>
-      </header>
-    </div>
-  );
+    <React.StrictMode>
+      <Header />
+      <Login />
+    </React.StrictMode>
+  )
 }
