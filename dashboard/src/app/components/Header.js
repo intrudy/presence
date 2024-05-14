dcd928b5bcbac54d4ab4831234a08a1cdbb0fc44
Harish Navnit <harishnavnit@gmail.com>
2024-05-14 18:06:48 +0800

    More header component refactors

--- dashboard/src/app/components/Header.js
+++ dashboard/src/app/components/Header.js
@@ -17,6 +17,17 @@ const callsToAction = [
 ]
 
 
+function Branding() {
+  return (
+    <div className="flex lg:flex-1">
+      <a href="#" className="-m-1.5 p-1.5">
+        <span className="sr-only">Turnout</span>
+        <img className="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="" />
+      </a>
+    </div>
+  )
+}
+
 function ReportsMenu() {
   return (
     <Popover className="relative">
@@ -78,12 +89,8 @@ export default function Header() {
   return (
     <header className="bg-white">
       <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
-        <div className="flex lg:flex-1">
-          <a href="#" className="-m-1.5 p-1.5">
-            <span className="sr-only">Turnout</span>
-            <img className="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="" />
-          </a>
-        </div>
+        <Branding />
+
         <div className="flex lg:hidden">
           <button
             type="button"
@@ -113,7 +120,7 @@ export default function Header() {
         <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
           <div className="flex items-center justify-between">
             <a href="#" className="-m-1.5 p-1.5">
-              <span className="sr-only">Your Company</span>
+              <span className="sr-only">Turnout</span>
               <img
                 className="h-8 w-auto"
                 src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
@@ -132,7 +139,10 @@ export default function Header() {
 
           <div className="mt-6 flow-root">
             <div className="-my-6 divide-y divide-gray-500/10">
+
               <div className="space-y-2 py-6">
+                <a href="#" className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Cohorts</a>
+                <a href="#" className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Events</a>
                 <Disclosure as="div" className="-mx-3">
                   {({ open }) => (
                     <>
@@ -158,19 +168,8 @@ export default function Header() {
                     </>
                   )}
                 </Disclosure>
-                <a
-                  href="#"
-                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                >
-                  Cohorts
-                </a>
-                <a
-                  href="#"
-                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                >
-                  Events
-                </a>
               </div>
+
               <div className="py-6">
                 <a
                   href="#"
@@ -179,6 +178,7 @@ export default function Header() {
                   Log in
                 </a>
               </div>
+
             </div>
           </div>
         </Dialog.Panel>
