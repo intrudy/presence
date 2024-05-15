69222f946f93db1985eeb2d2ba975fa442e08245
Harish Navnit <harishnavnit@gmail.com>
2024-05-15 10:47:19 +0800

    Define a mobile dialog component

--- dashboard/src/app/components/Header.js
+++ dashboard/src/app/components/Header.js
@@ -17,10 +17,61 @@ function Branding(props) {
   )
 }
 
+function MobileDialog({parent, active}) {
+  return(
+    <Dialog className="lg:hidden" open={active} onClose={() => parent.menuClose()}>
+      <div className="fixed inset-0 z-10" />
+      <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+        <div className="flex items-center justify-between">
+          <a href="#" className="-m-1.5 p-1.5">
+            <span className="sr-only">Turnout</span>
+            <img
+              className="h-8 w-auto"
+              src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+              alt=""
+            />
+          </a>
+          <button
+            type="button"
+            className="-m-2.5 rounded-md p-2.5 text-gray-700"
+            onClick={() => parent.menuClose()}
+          >
+            <span className="sr-only">Close menu</span>
+            <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+          </button>
+        </div>
+
+        <div className="mt-6 flow-root">
+          <div className="-my-6 divide-y divide-gray-500/10">
+            <div className="space-y-2 py-6">
+              <a href="#" className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Cohorts</a>
+              <a href="#" className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Events</a>
+              <Reports.Mobile />
+            </div>
+
+            <div className="py-6">
+              <a
+                href="#"
+                className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+              >
+                Log in
+              </a>
+            </div>
+          </div>
+        </div>
+      </Dialog.Panel>
+    </Dialog>
+  )
+}
+
 export default class Header extends React.Component {
   constructor(props) {
     super(props)
     this.state = {mobileMenuOpen: false}
+  }
+
+  menuClose() {
+    this.setState({mobileMenuOpen: false})
   }
 
   render() {
@@ -53,48 +104,7 @@ export default class Header extends React.Component {
           </div>
         </nav>
 
-        <Dialog className="lg:hidden" open={this.state.mobileMenuOpen} onClose={() => this.setState({mobileMenuOpen: false})}>
-          <div className="fixed inset-0 z-10" />
-          <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
-            <div className="flex items-center justify-between">
-              <a href="#" className="-m-1.5 p-1.5">
-                <span className="sr-only">Turnout</span>
-                <img
-                  className="h-8 w-auto"
-                  src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                  alt=""
-                />
-              </a>
-              <button
-                type="button"
-                className="-m-2.5 rounded-md p-2.5 text-gray-700"
-                onClick={() => this.setState({mobileMenuOpen: false})}
-              >
-                <span className="sr-only">Close menu</span>
-                <XMarkIcon className="h-6 w-6" aria-hidden="true" />
-              </button>
-            </div>
-
-            <div className="mt-6 flow-root">
-              <div className="-my-6 divide-y divide-gray-500/10">
-                <div className="space-y-2 py-6">
-                  <a href="#" className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Cohorts</a>
-                  <a href="#" className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Events</a>
-                  <Reports.Mobile />
-                </div>
-
-                <div className="py-6">
-                  <a
-                    href="#"
-                    className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                  >
-                    Log in
-                  </a>
-                </div>
-              </div>
-            </div>
-          </Dialog.Panel>
-        </Dialog>
+        <MobileDialog parent={this} active={this.state.mobileMenuOpen} />
       </header>
     )
   }
