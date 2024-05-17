25ca6886af7e90ca96f2247936f6e71662e87c7b
Harish Navnit <harishnavnit@gmail.com>
2024-05-17 23:52:27 +0800

    More links

--- apps/web/src/components/Dashboard.jsx
+++ apps/web/src/components/Dashboard.jsx
@@ -30,7 +30,7 @@ export function GearIcon() {
   )
 }
 
-export function Sidebar() {
+export function Menu() {
   return (
     <>
       <nav className="flex flex-col mx-4 my-6 space-y-4">
@@ -40,13 +40,13 @@ export function Sidebar() {
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z" />
           </svg>
         </Link>
-        <Link to={'/analytics'} className="inline-flex items-center justify-center py-3 text-purple-600 bg-white rounded-lg">
+        <Link to={'/reports/analytics'} className="inline-flex items-center justify-center py-3 text-purple-600 bg-white rounded-lg">
           <span className="sr-only">Analytics</span>
           <svg aria-hidden="true" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="h-6 w-6">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z" />
           </svg>
         </Link>
-        <Link href="#" className="inline-flex items-center justify-center py-3 hover:text-gray-400 hover:bg-gray-700 focus:text-gray-400 focus:bg-gray-700 rounded-lg">
+        <Link to={'/reports'} className="inline-flex items-center justify-center py-3 hover:text-gray-400 hover:bg-gray-700 focus:text-gray-400 focus:bg-gray-700 rounded-lg">
           <span className="sr-only">Reports</span>
           <svg aria-hidden="true" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="h-6 w-6">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M7 21h10a2 2 0 002-2V9.414a1 1 0 00-.293-.707l-5.414-5.414A1 1 0 0012.586 3H7a2 2 0 00-2 2v14a2 2 0 002 2z" />
@@ -57,13 +57,13 @@ export function Sidebar() {
   )
 }
 
-export function Navigator() {
+export function Sidebar() {
   return (
     <>
       <aside className="hidden sm:flex sm:flex-col">
         <Logo/>
         <div className="flex-grow flex flex-col justify-between text-gray-500 bg-gray-800">
-          <Sidebar/>
+          <Menu/>
           <div className="inline-flex items-center justify-center h-20 w-20 border-t border-gray-700">
             <GearIcon/>
           </div>
@@ -129,7 +129,7 @@ export default class Dashboard extends React.Component {
     return (
       <>
         <div className="flex h-full bg-gray-100">
-          <Navigator/>
+          <Sidebar/>
 
           <div className="flex-grow text-gray-800">
             <Header/>
