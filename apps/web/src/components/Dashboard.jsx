6d05ba663b4740917ca280b7e4e978d7b983f7f4
Harish Navnit <harishnavnit@gmail.com>
2024-05-17 19:57:42 +0800

    Nested routes and landing page

--- apps/web/src/components/Dashboard.jsx
+++ apps/web/src/components/Dashboard.jsx
@@ -1,11 +1,9 @@
 
 import React from "react"
-import { Link } from 'react-router-dom'
+import { Link, Outlet } from 'react-router-dom'
 
-import Charts from "./Charts"
 import Footer from "./Footer"
 import { Egg } from "./Shapes"
-import { Highlights, Leaderboard } from "./Stats"
 
 
 export function Logo() {
@@ -135,27 +133,9 @@ export default class Dashboard extends React.Component {
 
           <div className="flex-grow text-gray-800">
             <Header/>
-
-            <main className="p-6 sm:p-10 space-y-6">
-              <div className="flex flex-col space-y-6 md:space-y-0 md:flex-row justify-between">
-                <div className="mr-6">
-                  <h1 className="text-4xl font-semibold mb-2">Cohort Name</h1>
-                  <h2 className="text-gray-600 ml-0.5">A brief description of the Cohort</h2>
-                </div>
-                {/* <ManageSettings/> */}
-              </div>
-              <section className="grid md:grid-cols-2 xl:grid-cols-4 gap-6">
-                <Highlights/>
-              </section>
-
-              <section className="grid md:grid-cols-2 xl:grid-cols-4 xl:grid-rows-3 xl:grid-flow-col gap-6">
-                {/* <SecondaryStats/> */}
-                <Charts.Primary/>
-                <Leaderboard/>
-                <Charts.Secondary/>
-              </section>
-            </main>
-
+            <div id="content">
+              <Outlet/>
+            </div>
             <Footer.Minimal/>
           </div>
         </div>
