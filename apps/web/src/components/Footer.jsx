74eae157096d165989c8920573223552df8ce0e7
Harish Navnit <harishnavnit@gmail.com>
2024-05-17 18:43:35 +0800

    Fix footer padding

--- apps/web/src/components/Footer.jsx
+++ apps/web/src/components/Footer.jsx
@@ -7,7 +7,7 @@ export class Minimal extends React.Component {
     return (
       <>
         <section className="p-3 bg-white">
-          <div className="max-w-screen-xl px-2 py-2 mx-auto space-y-8 overflow-hidden sm:px-6 lg:px-8">
+          <div className="max-w-screen-xl px-4 py-4 mx-auto space-y-8 overflow-hidden sm:px-6 lg:px-8">
             <p className="text-base leading-normal text-center text-gray-400">
               © 2024 Turnout, Inc. Adapted from <a className="underline" href="https://dribbble.com/shots/10711741-Free-UI-Kit-for-Figma-Online-Courses-Dashboard">design</a> by Chili Labs.
             </p>
