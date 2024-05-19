9d996d917b4bc23149d7bd3717579184338ca5e1
Harish Navnit <harishnavnit@gmail.com>
2024-05-19 17:30:43 +0800

    Add a HTTP interface util

--- apps/web/src/utils.jsx
+++ apps/web/src/utils.jsx
@@ -2,3 +2,55 @@
 export function classNames(...classes) {
   return classes.filter(Boolean).join(' ')
 }
+
+export class Http {
+  constructor(serverUri) {
+    this.url = serverUri
+  }
+
+  headers() {
+    // TODO: Add authorization tokens
+    return {'X-FOO': 'BAR'}
+  }
+
+  async get(route, params={}) {
+    const r = await fetch(this.url + route + new URLSearchParams(params), {
+      method: 'GET',
+      headers: this.headers()
+    })
+    if (r.ok) { return r.json() }
+    throw new Error(`GET "${this.url + route}" failed with ${r.status}`)
+  }
+
+  async put(route, data={}) {
+    const r = await fetch(this.url + route, {
+      method: 'PUT',
+      headers: this.headers(),
+      body: JSON.stringify(data)
+    })
+
+    if (r.ok) { return r }
+    throw new Error(`PUT "${this.url + route}" failed with ${r.status}`)
+  }
+
+  async post(route, data={}) {
+    const r = await fetch(this.url + route, {
+      method: 'PUT',
+      headers: this.headers(),
+      body: JSON.stringify(data)
+    })
+
+    if (r.ok) { return r }
+    throw new Error(`POST "${this.url + route}" failed with ${r.status}`)
+  }
+
+  async delete(route) {
+    const r = await fetch(this.url + route, {
+        method: 'DELETE',
+        headers: this.headers()
+    })
+
+    if (r.ok) { return r }
+    throw new Error(`DELETE "${this.url + route}" failed with ${r.status}`)
+}
+}
