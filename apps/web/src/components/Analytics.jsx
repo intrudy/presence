d018a0046ab4f75ebfd48f34b471499251723177
Harish Navnit <harishnavnit@gmail.com>
2024-05-19 11:45:56 +0800

    Minor fix

--- apps/web/src/components/Analytics.jsx
+++ apps/web/src/components/Analytics.jsx
@@ -20,7 +20,7 @@ export default class Analytics extends React.Component {
   render() {
     return (
       <>
-        <main className="p-6 sm:p-10 space-y-6 relative isolate overflow-hidden bg-white">
+        <main className="min-h-full flex-1 p-6 sm:p-10 space-y-6 relative isolate overflow-hidden bg-white">
           <div className="absolute inset-0 -z-10 bg-[radial-gradient(45rem_50rem_at_top,theme(colors.indigo.100),white)] opacity-20" />
           <div className="absolute inset-y-0 right-1/2 -z-10 mr-16 w-[200%] origin-bottom-left skew-x-[-30deg] bg-white shadow-xl shadow-indigo-600/10 ring-1 ring-indigo-50 sm:mr-28 lg:mr-0 xl:mr-16 xl:origin-center" />
           <div className="mx-auto max-w max-h">
