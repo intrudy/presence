4c37438d4efb32616574158fc7a7b966072758bd
Harish Navnit <harishnavnit@gmail.com>
2024-05-19 11:37:20 +0800

    Login page gradient

--- apps/web/src/components/Entry.jsx
+++ apps/web/src/components/Entry.jsx
@@ -102,9 +102,11 @@ export default class Entry extends React.Component {
   render() {
     return (
       <>
-        <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+        <div className="min-h-full flex-1 p-6 sm:p-10 space-y-6 relative isolate overflow-hidden bg-white">
           <EntryHeader />
 
+          <div className="absolute inset-0 -z-10 bg-[radial-gradient(45rem_50rem_at_top,theme(colors.indigo.100),white)] opacity-20" />
+          <div className="absolute inset-y-0 right-1/2 -z-10 mr-16 w-[200%] origin-bottom-left skew-x-[-30deg] bg-white shadow-xl shadow-indigo-600/10 ring-1 ring-indigo-50 sm:mr-28 lg:mr-0 xl:mr-16 xl:origin-center" />
           <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
             <SigninForm />
             <Registration />
