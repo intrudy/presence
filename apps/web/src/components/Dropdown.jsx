57cf8056376ecd3da6cde72369c2565a26a9e6a6
Harish Navnit <harishnavnit@gmail.com>
2024-11-17 12:15:40 +0800

    Fix dropdown component

--- apps/web/src/components/Dropdown.jsx
+++ apps/web/src/components/Dropdown.jsx
@@ -1,7 +1,7 @@
 
 import { Fragment } from 'react'
 import { Link } from 'react-router-dom'
-import { Menu, Transition } from '@headlessui/react'
+import { Menu, MenuButton, MenuItem, MenuItems, Transition } from '@headlessui/react'
 import { ChevronDownIcon } from '@heroicons/react/20/solid'
 
 import { classNames } from '../utils'
@@ -11,10 +11,10 @@ export default function Dropdown({cohorts}) {
   return (
     <Menu as="div" className="relative inline-block text-right">
       <div>
-        <Menu.Button className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
+        <MenuButton className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
           Cohorts
           <ChevronDownIcon className="-mr-1 h-5 w-5 text-gray-400" aria-hidden="true" />
-        </Menu.Button>
+        </MenuButton>
       </div>
 
       <Transition
@@ -26,10 +26,10 @@ export default function Dropdown({cohorts}) {
         leaveFrom="transform opacity-100 scale-100"
         leaveTo="transform opacity-0 scale-95"
       >
-        <Menu.Items className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+        <MenuItems className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
           <div className="py-1">
             {cohorts.map((cohort) => (
-              <Menu.Item key={cohort.name}>
+              <MenuItem key={cohort.name}>
                 {({ active }) => (
                   <Link
                     to={'/cohorts/' + cohort.id}
@@ -41,10 +41,10 @@ export default function Dropdown({cohorts}) {
                     {cohort.name}
                   </Link>
                 )}
-              </Menu.Item>
+              </MenuItem>
             ))}
           </div>
-        </Menu.Items>
+        </MenuItems>
       </Transition>
     </Menu>
   )
